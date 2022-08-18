cec524dee8ca1627db97a2824da8dca6bb3413ea
jakos <jakosvadim@gmail.com>
2022-08-18 12:30:28 +0300

    Added logo

--- minesweeper.pas
+++ minesweeper.pas
@@ -4,6 +4,17 @@ uses game, MainMenu, GameResults, widget, keyboard, crt, sysutils;
 
 const
     NewRecordTitle = 'New record!';
+    LogoWidth = 60;
+    LogoHeight = 6;
+    logo: array [1..LogoHeight] of string = (
+        ' __  __ _'#10,
+        '|  \/  (_)_ __   ___  _____      _____  ___ _ __   ___ _ __'#10,
+        '| |\/| | | ''_ \ / _ \/ __\ \ /\ / / _ \/ _ \ ''_ \ / _ \ ''__|'#10,
+        '| |  | | | | | |  __/\__ \\ V  V /  __/  __/ |_) |  __/ |'#10,
+        '|_|  |_|_|_| |_|\___||___/ \_/\_/ \___|\___| .__/ \___|_|'#10,
+        '                                           |_|'
+    );
+
 procedure ProceedGameResults(var GameState: TGame);
 var
     GameTime: TDateTime;
@@ -24,6 +35,20 @@ begin
     end;
 end;
 
+procedure PrintLogo;
+var
+    i, x, y: shortint;
+begin
+    x := (ScreenWidth - LogoWidth) div 2 + 1;
+    y := ScreenHeight div 2 + 1 - 2 * LogoHeight;
+    for i := 1 to LogoHeight do
+    begin
+        GotoXY(x, y + i - 1);
+        write(logo[i]);
+    end;
+    GotoXY(1, 1);
+end;
+
 var
     GameState: TGame;
     SelectedButton: MenuButton;
@@ -31,9 +56,11 @@ begin
     GameState.IsRestart := false;
     while true do
     begin
+        clrscr;
         if not GameState.IsRestart then
         begin
             SelectedButton := BNone;
+            PrintLogo;
             ShowMenu(SelectedButton);
         end;
         case SelectedButton of 
